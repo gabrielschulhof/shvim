27cf0b65a0a62ca6b8bf022743f0c318eb492b7b
Gabriel Schulhof <gabrielschulhof@gmail.com>
2021-05-25 23:51:10 -0700

    enable debug to file

--- src/debug.c
+++ src/debug.c
@@ -3,9 +3,11 @@
 #include "debug.h"
 
 void debug(const char* fmt, ...) {
+  DEBUG(FILE* output = fopen("/home/gschulhof/shvim/shvim.log", "a"));
   DEBUG(va_list va);
   DEBUG(va_start(va, fmt));
-  DEBUG(vfprintf(stderr, fmt, va));
-  DEBUG(fflush(stderr));
+  DEBUG(vfprintf(output, fmt, va));
+  DEBUG(fflush(output));
+  DEBUG(fclose(output));
   DEBUG(va_end(va));
 }
