370527870bd138dcd62da5b8d965b7a797f5235e
Gabriel Schulhof <gabrielschulhof@gmail.com>
2023-06-17 22:05:59 -0700

    all: abort on log file open failure

--- src/debug.c
+++ src/debug.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
 #include "debug.h"
 
 void debug(const char* fmt, ...) {
   FILE* pfile = fopen("/Users/gschulhof/dev/shvim/log", "a");
+  if (!pfile) {
+    fprintf(stderr, "Failed to open log file\n");
+    abort();
+  }
   DEBUG(va_list va);
   DEBUG(va_start(va, fmt));
   DEBUG(vfprintf(pfile, fmt, va));
